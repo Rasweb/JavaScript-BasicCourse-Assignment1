2fce1a04d8a8339cc557b23cd00b4b7508d3ef0a
RasWeb <rasmuspalm@fastmail.se>
2021-11-02 12:51:38 +0100

    basic commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-window.onload = function () {};
+window.onload = function () {
+  let remBtn = document.getElementById("rem");
+  remBtn.addEventListener("click", remItem);
+
+  let testing = document.getElementById("test");
+  testing.addEventListener("click", remvEt);
+};
+
+function remvEt() {
+  if (e.target.classList.contains("check")) {
+    list.removeChild(e.target.parentElement);
+    list.removeChild(list);
+  }
+}
+
+// function remItem() {
+//   //for (let i = 0; i < li.length; i++){}
+//   // alert("Hello");
+// }
