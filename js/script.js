51e5f24a7723f2c35417d317ec285529c1c65a40
RasWeb <rasmuspalm@fastmail.se>
2021-11-02 15:33:48 +0100

    fixed the checked javascript

--- js/script.js
+++ js/script.js
@@ -1,41 +1,28 @@
+window.onload = function () {
+  closefunc();
+  checkfunc();
+};
+
 // Click on the element with class="close".
 // Changes the list items display to none.
-let close = document.getElementsByClassName("close");
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function () {
-    let div = this.parentElement;
-    div.style.display = "none";
-  };
+function closefunc() {
+  let close = document.getElementsByClassName("close");
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      let div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
 }
 
 // Add a "checked" symbol when clicking on a list item
-let list = document.querySelector("ul");
-list.addEventListener(
-  "click",
-  function (e) {
+// querySelector returns the first element that matches.
+function checkfunc() {
+  let list = document.querySelector("ul");
+  list.addEventListener("click", function (e) {
     if (e.target.tagName === "LI") {
+      // Returns the list, its allways in uppercase
       e.target.classList.toggle("checked");
     }
-  },
-  false
-);
-
-// window.onload = function () {
-//   let remBtn = document.getElementById("rem");
-//   remBtn.addEventListener("click", remItem);
-
-//   let testing = document.getElementById("test");
-//   testing.addEventListener("click", remvEt);
-// };
-
-// function remvEt() {
-//   if (e.target.classList.contains("check")) {
-//     list.removeChild(e.target.parentElement);
-//     list.removeChild(list);
-//   }
-// }
-
-// function remItem() {
-//   //for (let i = 0; i < li.length; i++){}
-//   // alert("Hello");
-// }
+  });
+}
