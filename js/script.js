b1f1075eb834770ca54d485aa1edba0cce9e7c48
RasWeb <rasmuspalm@fastmail.se>
2021-11-02 15:00:08 +0100

    Working remove click.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
-window.onload = function () {
-  let remBtn = document.getElementById("rem");
-  remBtn.addEventListener("click", remItem);
-
-  let testing = document.getElementById("test");
-  testing.addEventListener("click", remvEt);
-};
-
-function remvEt() {
-  if (e.target.classList.contains("check")) {
-    list.removeChild(e.target.parentElement);
-    list.removeChild(list);
-  }
+// Click on the element with class="close".
+// Changes the list items display to none.
+let close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    let div = this.parentElement;
+    div.style.display = "none";
+  };
 }
+
+// window.onload = function () {
+//   let remBtn = document.getElementById("rem");
+//   remBtn.addEventListener("click", remItem);
+
+//   let testing = document.getElementById("test");
+//   testing.addEventListener("click", remvEt);
+// };
+
+// function remvEt() {
+//   if (e.target.classList.contains("check")) {
+//     list.removeChild(e.target.parentElement);
+//     list.removeChild(list);
+//   }
+// }
 
 // function remItem() {
 //   //for (let i = 0; i < li.length; i++){}
