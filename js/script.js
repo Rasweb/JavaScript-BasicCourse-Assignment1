291620bb25a123e6ced1436af57799ae0ab01b40
RasWeb <rasmuspalm@fastmail.se>
2021-11-04 11:37:40 +0100

    removed unnecessary code.

--- js/script.js
+++ js/script.js
@@ -5,18 +5,6 @@ window.onload = function () {
   liAdd();
 };
 
-// Click on the element with class="close".
-// Changes the list items display to none.
-function closefunc() {
-  let close = document.getElementsByClassName("close");
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      let div = this.parentElement;
-      div.style.display = "none";
-    };
-  }
-}
-
 // Add a "checked" symbol when clicking on a list item
 // querySelector returns the first element that matches.
 function checkfunc() {
@@ -30,16 +18,27 @@ function checkfunc() {
   });
 }
 
+// Adds the list items dynamically and the p tag with class.
 function liAdd() {
   let tagP = document.createElement("p"); // Creates <p>
-  let textP = document.createTextNode("x"); // Adds x
-  tagP.appendChild(textP); // Appends <p>x<p>
+  tagP.appendChild(document.createTextNode("x")); // Appends <p>x<p>
   let theUl = document.getElementById("liUl"); // Finds the <ul>.
   let theInpt = document.getElementById("liInput"); // Finds the <input>.
   let theLi = document.createElement("li"); // Creates the <li>.
-  //theLi.setAttribute("id", theInpt.value); // Adds the input value and Id.
   theLi.appendChild(document.createTextNode(theInpt.value)); // Appends the input value using a textnode.
   theLi.appendChild(tagP); // Appends the <p> to <li>
   tagP.setAttribute("class", "close"); // Adds class="close"
   theUl.appendChild(theLi); // Appends to the ul to make a list
+}
+
+// Click on the element with class="close".
+// Changes the list items display to none.
+function closefunc() {
+  let close = document.getElementsByClassName("close");
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      let div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
 }
