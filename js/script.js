716193d5a98f007c73dd46a2eaf33519a6e025c5
RasWeb <rasmuspalm@fastmail.se>
2021-11-09 15:14:29 +0100

    simple add and remove button

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 window.onload = function () {
+  document.getElementById("add").addEventListener("click", addButton);
+  document.getElementById("remove").addEventListener("click", removeButton);
+  jsLists();
   check();
-  document.getElementById("add").addEventListener("click", liAdd);
-  document.getElementById("remove").addEventListener("click", removeItem);
-  document.getElementById("sort").addEventListener("click", sort);
+  // jsAdd();
+  // jsRemove();
 };
 
-lists();
-add();
+// Create the Ul.
+let jsUl = document.createElement("ul");
+jsUl.id = "liUl";
 
-function lists() {
+function jsLists() {
   //The list.
-  let jsLists = [
+  let jsList = [
     "Pay bills",
     "Study",
     "Train",
@@ -18,41 +21,16 @@ function lists() {
     "Read book",
     "Clean",
   ];
-  let jsUl = document.createElement("ul");
-  // Id test.
-  jsUl.id = "Test";
 
-  for (let i = 0; i < jsLists.length; i++) {
-    // Loops through the list and appends to ul.
+  for (let i = 0; i < jsList.length; i++) {
+    // Find the main tag.
+    let main = document.getElementById("main");
+    // Create the div, ul and li tag.
     let jsLi = document.createElement("li");
-    jsLi.innerHTML = jsLists[i];
+    jsLi.innerHTML = jsList[i];
     jsUl.appendChild(jsLi);
-    document.body.appendChild(jsUl);
+    main.appendChild(jsUl);
   }
-}
-
-function add() {
-  let addB = document.getElementById("TestButton");
-  let addInput = document.getElementById("TestInput");
-}
-
-function remove() {
-  let remB = document.getElementsById("rembutton");
-}
-
-// -------------------------------------------------------------//
-
-function removeItem() {
-  // Finds the ul and the list item to change.
-  let listItems = document.getElementById("liUl").getElementsByTagName("li");
-  // Finds the remove button.
-  let removeButton = document.getElementById("remove");
-
-  //loops through the list backwards, removes last item.
-  if (listItems.length === 0) return;
-
-  let last = listItems[listItems.length - 1];
-  last.parentNode.removeChild(last);
 }
 
 // Add a "checked" symbol when clicking on a list item
@@ -67,41 +45,41 @@ function check() {
   });
 }
 
-//Adds the list items dynamically and the span tag with class.
-function liAdd() {
-  let theUl = document.getElementById("liUl"); // Finds the <ul>.
-  let theInpt = document.getElementById("liInput"); // Finds the <input>.
-  let theLi = document.createElement("li"); // Creates the <li>.
-  theLi.appendChild(document.createTextNode(theInpt.value)); // Appends the input value using a textnode.
-  theUl.appendChild(theLi); // Appends to the ul to make a list
+// Add Button
+let divControl = document.createElement("div");
+divControl.id = "control";
+
+function addButton() {
+  let addB = document.getElementById("add"); // Finds the add button.
+  let addUl = document.getElementById("liUl"); // Finds the ul.
+  let inpB = document.getElementById("input"); // Finds the input button.
+  let nLi = document.createElement("li"); // Creates the li.
+  nLi.appendChild(document.createTextNode(inpB.value)); // Appends the input value.
+  addUl.appendChild(nLi); // Appends the li to ul.
+
+  // for (let i = 0; i < liUl.length; i++) {
+
+  // }
 }
 
-function sort() {
-  // Empty variables
-  let run, stop;
-
-  // Loop will continue until sort has been done.
-  let sort_list = document.getElementById("liUl");
-  run = true;
-
-  while (run) {
-    run = false;
-    // li is the <li>.
-    let li = sort_list.getElementsByTagName("LI");
-    // Loop through the list.
-    for (i = 0; i < li.length - 1; i++) {
-      stop = false;
-      if (li[i].innerHTML.toLowerCase() > li[i + 1].innerHTML.toLowerCase()) {
-        // Sorting the items in alphabeticall order.
-        // If item should be sorted mark as stop and break.
-        stop = true;
-        break;
-      }
-    }
-    if (stop) {
-      li[i].parentNode.insertBefore(li[i + 1], li[i]);
-      // If item has been marked with stop sort the item.
-      run = true;
-    }
+// Remove button
+function removeButton() {
+  // Finds the remove button.
+  let remB = document.getElementById("remove");
+  // Finds the ul and the list item to change.
+  let listItems = document.getElementById("liUl").getElementsByTagName("li");
+  //loops through the list backwards, removes last item.
+  if (listItems.length === 0) return;
+  {
+    let last = listItems[listItems.length - 1];
+    last.parentNode.removeChild(last);
   }
 }
+
+// <form>
+// <input type="text" onfocus="this.value=''" id="liInput" />
+// <!-- onfocus clears the input field on click. -->
+// </form>
+// <button type="text" id="sort">Sort</button>
+
+// -------------------------------------------------------------//
